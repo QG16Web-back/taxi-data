543459b992553d2590f5c6bca86d2e04e5880697
WilderGao <845956752@qq.com>
2018-08-05 22:38:00 +0800

    高键城部分可视化

---
+++ src/main/java/com/qg/taxi/service/impl/DiagramServiceImpl.java
@@ -0,0 +1,10 @@
+package com.qg.taxi.service.impl;
+
+/**
+ * @author Wilder Gao
+ * time:2018/8/5
+ * description：
+ * motto: All efforts are not in vain
+ */
+public class DiagramServiceImpl {
+}
---
+++ src/main/java/com/qg/taxi/web/GeoHashController.java
@@ -0,0 +1,10 @@
+package com.qg.taxi.web;
+
+/**
+ * @author Wilder Gao
+ * time:2018/8/5
+ * description：
+ * motto: All efforts are not in vain
+ */
+public class GeoHashController {
+}
